4319a0a5fcfa01cb7afeab8b244c25bf00671e87
hyrki09 <hyrki0901@naver.com>
2026-03-14 15:43:29 +0900

    feat : 여러 코인 동시 수집 기능 추가

--- collector.py
+++ collector.py
@@ -1,7 +1,15 @@
 import pyupbit
 
-price = pyupbit.get_current_price("KRW-BTC")
+# 시세화깅ㄴ
+# price = pyupbit.get_current_price("KRW-BTC")
 
-print(f"비트코인 현재가 :  {int(price):,}원")
+# print(f"비트코인 현재가 :  {int(price):,}원")
 
+
+# 수집할 코인 목록
+tickers = ["KRW-BTC", "KRW-ETH", "KRW-XRP"]
+
+for ticker in tickers:
+    price = pyupbit.get_current_price(ticker)
+    print(f"[{ticker}] {int(price):,}원")
 

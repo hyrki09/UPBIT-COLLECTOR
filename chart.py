d2bf5b754a68bed66ba90d0eb8fd8cc400039bd2
hyrki09 <hyrki0901@naver.com>
2026-03-16 22:53:51 +0900

    feat : 여러 코인 차트 한 번에 보기 추가

--- chart.py
+++ chart.py
@@ -16,7 +16,7 @@ def draw_char(ticker):
     df['ma7'] = df['close'].rolling(7).mean()
     df['ma30'] = df['close'].rolling(30).mean()
 
-    print(df)
+    # print(df)
 
     fig, ax = plt.subplots(figsize=(12, 5)) 
 
@@ -29,10 +29,48 @@ def draw_char(ticker):
     ax.legend()
     ax.grid(True) # 격자 표시
 
-    print(fig)
+    # print(fig)
 
     plt.tight_layout()
     plt.show()
 
+#* 여러 코인 차트 한번에 보기
+def draw_multiple_charts(tickers):
+    fig, axes = plt.subplots(len(tickers), 1, figsize=(12, 5 * len(tickers)))
+
+    for i, ticker in enumerate(tickers):
+        df = load_data(ticker)
+        
+
+        # 이동평균 계산
+        df['ma5'] = df['close'].rolling(5).mean()
+        df['ma10'] = df['close'].rolling(10).mean()
+        df['ma20'] = df['close'].rolling(20).mean()
+        df['ma60'] = df['close'].rolling(60).mean()
+        df['ma120'] = df['close'].rolling(120).mean()
+
+
+        # print(df)
+        
+
+        ax = axes[i]
+        ax.plot(df.index, df['close'], label='종가', color='blue', alpha=0.5)
+        ax.plot(df.index, df['ma5'], label='5일 이동평균', color='green')
+        ax.plot(df.index, df['ma10'], label='10일 이동평균', color='black')
+        ax.plot(df.index, df['ma20'], label='20일 이동평균', color='cyan')
+        ax.plot(df.index, df['ma60'], label='60일 이동평균', color='brown')
+        ax.plot(df.index, df['ma120'], label='120일 이동평균', color='midnightblue')
+
+        ax.set_title(f"{ticker} 가격차트")
+        ax.set_xlabel("날짜")
+        ax.set_ylabel("가격 (원)")
+        ax.legend()
+        ax.grid(True)
+    # print(axes)
+    plt.tight_layout()
+    plt.show()
+
+
 if __name__ == "__main__":
-    draw_char("KRW-BTC")
+    # draw_char("KRW-BTC")
+    draw_multiple_charts(["KRW-BTC", "KRW-ETH", "KRW-XRP"])
